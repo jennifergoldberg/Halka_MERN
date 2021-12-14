cb8ccf81d15c34899fe8caf1cd51c651db0b1f97
lonnalu <lonnalu@gmail.com>
2021-12-14 12:08:16 -0800

    Adds Signup component for testing

--- halka/src/pages/App/App.js
+++ halka/src/pages/App/App.js
@@ -1,13 +1,11 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import SignUp from '../SignUpPage/SignUp';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<SignUp />} />
-      </Routes>
-    </BrowserRouter>
+    <div className='App'>
+      <h1>Hello</h1>
+      <SignUp />
+    </div>
   );
 }
 
--- halka/src/pages/SignUpPage/SignUp.jsx
+++ halka/src/pages/SignUpPage/SignUp.jsx
@@ -1,1 +1,9 @@
 import React, { useState } from 'react';
+
+function SignUp() {
+  return (
+    <h2>Sign Up for Halka!</h2>
+  )
+}
+
+export default SignUp;
