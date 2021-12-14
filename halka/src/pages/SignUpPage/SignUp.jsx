ffe880b592d1d7478b83d4ebc6bc948708ff71df
jennifergoldberg <jagoldbe@gmail.com>
2021-12-14 15:43:52 -0500

    before signup merge

--- halka/src/pages/SignUpPage/SignUp.jsx
+++ halka/src/pages/SignUpPage/SignUp.jsx
@@ -1,17 +1,9 @@
 import React, { useState } from 'react';
 
 function SignUp() {
-<<<<<<< HEAD
-  return(
-    <h1>SignUp</h1>
-  )
-
-};
-=======
   return (
     <h2>Sign Up for Halka!!</h2>
   )
 }
->>>>>>> dev
 
 export default SignUp;
