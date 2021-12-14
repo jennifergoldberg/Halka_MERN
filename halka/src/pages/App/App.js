8c20f7226e388e7c90aff52a0a8a5befbbdabd90
lonnalu <lonnalu@gmail.com>
2021-12-14 12:17:42 -0800

    Adds react router dom

--- halka/src/pages/App/App.js
+++ halka/src/pages/App/App.js
@@ -1,10 +1,14 @@
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import SignUp from '../SignUpPage/SignUp';
 
 function App() {
   return (
     <div className='App'>
-      <h1>Hello</h1>
-      <SignUp />
+      <h1>Halka</h1>
+      <Routes>
+        <Route path='/' element={<SignUp /> } />
+      </Routes>
     </div>
   );
 }
--- halka/src/pages/SignUpPage/SignUp.jsx
+++ halka/src/pages/SignUpPage/SignUp.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 
 function SignUp() {
   return (
-    <h2>Sign Up for Halka!</h2>
+    <h2>Sign Up for Halka!!</h2>
   )
 }
 
