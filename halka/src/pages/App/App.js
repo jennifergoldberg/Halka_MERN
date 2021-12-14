55881fd9d94d924c0c98b6337f5206a017285489
jennifergoldberg <jagoldbe@gmail.com>
2021-12-14 16:00:45 -0500

    login page

--- halka/src/pages/App/App.js
+++ halka/src/pages/App/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import SignUp from '../SignUpPage/SignUp';
+import Login from '../LoginPage/Login'
 
 function App() {
   return (
     <div className='App'>
       <h1>Halka</h1>
       <Routes>
-        <Route path='/' element={<SignUp /> } />
+        <Route path='/signup' element={<SignUp /> } />
+        <Route path='/login' element={<Login /> } />
       </Routes>
     </div>
   );
