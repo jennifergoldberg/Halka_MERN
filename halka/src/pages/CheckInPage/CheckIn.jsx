0babb2f59fdf14d23bb613610403750d404cd147
jennifergoldberg <jagoldbe@gmail.com>
2021-12-14 19:29:18 -0500

    git status

--- halka/src/pages/CheckInPage/CheckIn.jsx
+++ halka/src/pages/CheckInPage/CheckIn.jsx
@@ -1,16 +1,18 @@
 import React, { useState } from 'react'
 import Responses from '../../components/Responses';
 
-function CheckIn() {
+function CheckIn(props) {
 
   const [responseCount, setResponseCount] = useState(0)
 
-
+  
 
   return (
     <div>
       <h1>CheckIn</h1>
-      <Responses onClick={() => setResponseCount(responseCount + 1)}/>
+      <Responses 
+        onClick={() => setResponseCount(responseCount + props.value)}
+      />
       {console.log(responseCount)}
     </div>
 
