0b7d77346a8f241b2d4796873a870482a1cdd2e9
lonnalu <lonnalu@gmail.com>
2021-12-15 08:39:53 -0800

    Stores each answer and saves the average

--- halka/src/components/CheckInFooter.jsx
+++ halka/src/components/CheckInFooter.jsx
@@ -1,19 +1,35 @@
 import React from 'react'
 import { Button, Icon } from 'semantic-ui-react'
 
-const CheckInFooter = (props) => (
-  <div>
-    {props.currentQuestion === 1 ? <></> : <Button icon>
-      <Icon name='chevron circle left' onClick={props.decrementQuestionCount}/>
-    </Button>}
+const CheckInFooter = (props) => {
+  // {!props.selectedResponse && 'disabled'}
 
-    { props.currentQuestion === 5 ? 
-      <Button style={{backgroundColor:"#101340", color:"white"}} type='submit'>Submit</Button>
-    : <Button icon>
-      <Icon name='chevron circle right' onClick={props.incrementQuestionCount}/>
-    </Button> 
-    }
-  </div>
-)
+  return(
+    <div>
+      {props.currentQuestion === 1 ? <></> : <Button icon>
+        <Icon name='chevron circle left' onClick={props.decrementQuestionCount}/>
+      </Button>}
+
+      { props.currentQuestion === 5 ? 
+        <Button 
+          style={{backgroundColor:"#101340", color:"white"}}
+          type='submit' 
+          className={props.selectedResponse ? '' : 'disabled'}
+          onClick={() => {
+            props.handleSaveResponse();
+            props.handleSubmit();
+          }} >Submit</Button>
+      : <Button icon className={props.selectedResponse ? '' : 'disabled'}>
+        <Icon 
+          name='chevron circle right' 
+          onClick={() => {
+            props.incrementQuestionCount(); 
+            props.handleSaveResponse() 
+          }}/>
+      </Button> 
+      }
+    </div>
+  )
+}
 
 export default CheckInFooter
--- halka/src/components/Response.jsx
+++ halka/src/components/Response.jsx
@@ -1,10 +1,12 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Button } from 'semantic-ui-react'
 
-const Response = (props) => (
+const Response = (props) => {
+  return (
   <div>
-    <Button style={{width:"200px", marginBottom:"20px"}} onHover={{color:"#FFC700"}} content={props.content} value={props.value} basic />
+    <Button style={{width:"200px", marginBottom:"20px", marginLeft:"40px"}} content={props.content} value={props.value} basic onClick={(e) => props.handleResponseClick(e.target.value)} />
   </div>
-)
+  )
+}
 
 export default Response;
